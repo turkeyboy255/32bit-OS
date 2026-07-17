f75f1cc9c89fc7d3f89c0b8adf5405a7285812e5
turkeyboy255 <turkey.boyz2010@gmail.com>
2026-07-16 22:21:35 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 nasm -f bin Bootloader.asm -o Bootloader.bin
 nasm -f bin Kernel32.asm -o Kernel32.bin
+nasm -f bin filetable.asm -o filetable.bin
+
+
 
 dd if=/dev/zero of=os.img bs=512 count=2880
 
 dd if=Bootloader.bin of=os.img bs=512 count=1 conv=notrunc
 dd if=Kernel32.bin of=os.img bs=512 seek=1 conv=notrunc
 
-echo "HELLO FROM DISK" > test.txt
-dd if=test.txt of=os.img bs=512 seek=20 conv=notrunc
+dd if=filetable.bin of=os.img bs=512 seek=33 conv=notrunc
 
 qemu-system-i386 -drive format=raw,file=os.img
