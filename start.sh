30fa74e8220a0f250059f85d7d1e9187613c6185
turkeyboy255 <turkey.boyz2010@gmail.com>
2026-07-16 21:16:34 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,7 @@ dd if=/dev/zero of=os.img bs=512 count=2880
 dd if=Bootloader.bin of=os.img bs=512 count=1 conv=notrunc
 dd if=Kernel32.bin of=os.img bs=512 seek=1 conv=notrunc
 
+echo "HELLO FROM DISK" > test.txt
+dd if=test.txt of=os.img bs=512 seek=20 conv=notrunc
+
 qemu-system-i386 -drive format=raw,file=os.img
