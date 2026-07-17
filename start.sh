b59f7f16a8e8b7898fee5c433c4686ca8a3e73fc
turkeyboy255 <turkey.boyz2010@gmail.com>
2026-07-16 20:24:54 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 nasm -f bin Bootloader.asm -o Bootloader.bin
 nasm -f bin Kernel32.asm -o Kernel32.bin
-dd if=Bootloader.bin of=os.img bs=512 count=1
+
+dd if=/dev/zero of=os.img bs=512 count=2880
+
+dd if=Bootloader.bin of=os.img bs=512 count=1 conv=notrunc
 dd if=Kernel32.bin of=os.img bs=512 seek=1 conv=notrunc
+
 qemu-system-i386 -drive format=raw,file=os.img
