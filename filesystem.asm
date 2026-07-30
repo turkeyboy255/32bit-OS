7fe4f828d2379b6e67e09a354ffa8016992c49a1
turkeyboy255 <turkey.boyz2010@gmail.com>
2026-07-29 22:53:21 -0500

    Update filesystem.asm

--- filesystem.asm
+++ filesystem.asm
@@ -1,16 +1,16 @@
 filesystem:
 
-db 'JFS1'
+db 'HEFS'
 
-dd 2              ; 2 files
+dd 4              ; 2 files
 
 
 ; ----------------
 ; FILE 1
 ; ----------------
 
-db 'BOOTLOADER'
-times 6 db 0   ; filename padding
+db 'BOOTLOADER '
+times 5 db 0   ; filename padding
 
 dd 0            ; starting sector
 dd 512           ; size bytes
@@ -22,14 +22,29 @@ dd 0             ; reserved
 ; FILE 2
 ; ----------------
 
-db 'KERNEL'
-times 10 db 0
+db 'KERNEL '
+times 9 db 0
 
 dd 1
 dd 16384
 dd 0
 dd 0
 
+db 'FILETABLE '
+times 6 db 0
+
+dd 33
+dd 512
+dd 0
+dd 0
+
+db 'HELLOWORLD '
+times 5 db 0
+
+dd 34
+dd 512
+dd 0
+dd 0
 
 ; fill sector
 times 512-($-$$) db 0
