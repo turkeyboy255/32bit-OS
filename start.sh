b22a7939a3c84fcba41abe2f4ce8694a9698f4eb
turkeyboy255 <turkey.boyz2010@gmail.com>
2026-07-29 22:52:07 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,27 @@
 nasm -f bin Bootloader.asm -o Bootloader.bin
 nasm -f bin Kernel32.asm -o Kernel32.bin
 nasm -f bin filetable.asm -o filetable.bin
+nasm -f bin hello.asm -o hello.bin
+
+
+dd if=/dev/zero of=os.img bs=512 count=2880
+
+# boot sector
+dd if=Bootloader.bin of=os.img bs=512 seek=0 conv=notrunc
+
+# kernel
+dd if=Kernel32.bin of=os.img bs=512 seek=1 conv=notrunc
+
+# HEFS file table
+dd if=filetable.bin of=os.img bs=512 seek=33 conv=notrunc
+
+# HELLOWORLD program
+dd if=hello.bin of=os.img bs=512 seek=34 conv=notrunc
+
+
+qemu-system-i386 -drive format=raw,file=os.imgnasm -f bin Bootloader.asm -o Bootloader.bin
+nasm -f bin Kernel32.asm -o Kernel32.bin
+nasm -f bin filetable.asm -o filetable.bin
 
 
 
