c00dc7d1b5e2176d0cd69a33a998df47e1d07971
turkeyboy255 <turkey.boyz2010@gmail.com>
2026-08-02 13:57:47 -0500

    Update Kernel32.asm

--- Kernel32.asm
+++ Kernel32.asm
@@ -2,17 +2,18 @@ org 0x7E00
 BITS 32
 
 start:
-    mov eax,33  ;loads the last saved file table to 0x20000 in memory
-    mov edi,0x20000
-    call ata_read_sector
+    mov eax,33              ; filetable is stored at sector 33 on the disk
+    mov edi,0x20000         ; where filetable is loaded for later refrence
+    call ata_read_sector    ; loads the filetable
 
-    jmp get_inp
+    jmp get_inp             ; runs the basic command line
 
 ; ==========================
 ; KEYBOARD INPUT
 ; ==========================
 get_inp:
-.wait:
+.wait:                      ; read keys loop
+    ; waits till key pressed
     in al,0x64
     test al,1
     jz .wait
@@ -23,6 +24,7 @@ get_inp:
     test al,0x80
     jnz get_inp
 
+    ; special keys
     cmp al,0x1C
     je enter_key
 
@@ -34,11 +36,12 @@ get_inp:
 
     ; letters
     movzx ebx,al
-    mov al,[keymap+ebx]
+    mov al,[keymap+ebx]     ; uses the keymap to find the correct key to load
 
     cmp al,0
     je get_inp
 
+    ; prints characters to the screen then continues the loop
     call print_char
     jmp get_inp
 
@@ -51,15 +54,16 @@ enter_key:
     movzx edi,byte [buffer_pos]
     mov byte [buffer+edi],0
 
-    call command
-    call clear_buffer
+    call command            ; compares the buffer to stored commands to execute
+    call clear_buffer       ; clears the buffer
 
-    jmp get_inp
+    jmp get_inp             ; continues the command line loop
 
 ; ==========================
 ; BACKSPACE
 ; ==========================
 backspace:
+    ; reoves the last entry from the buffer and backs the pointer up
     cmp byte [buffer_pos],0
     je get_inp
 
@@ -77,6 +81,7 @@ backspace:
 ; SPACE
 ; ==========================
 spacebar:
+    ; i forgot to add spaces to the keymap so i just added a subroutine
     mov al,' '
     call print_char
     jmp get_inp
@@ -86,11 +91,12 @@ spacebar:
 ; PRINT CHARACTER
 ; ==========================
 print_char:
+    ; loads the last pressed character to the screen then stores it to the buffer
     push eax
-    mov edi,[cursor]
-    mov ah,0x0F
-    mov [edi],ax
-    add dword [cursor],2
+    mov edi,[cursor]            ; starts the cursor to track the point on the screen
+    mov ah,0x0F                 
+    mov [edi],ax                
+    add dword [cursor],2        ; moves the cursor to keep track of the screen
     pop eax
     call store_char
     ret
@@ -100,14 +106,16 @@ print_char:
 ; STORE INPUT
 ; ==========================
 store_char:
-    movzx edi,byte [buffer_pos]
-    mov [buffer+edi],al
-    inc byte [buffer_pos]
+    ; stores the last pressed character to the buffer
+    movzx edi,byte [buffer_pos] ; allocates one byte for the character
+    mov [buffer+edi],al         ; stores the character
+    inc byte [buffer_pos]       ; increments the buffer pointer
     ret
 
 ; ==========================
 ; COMMAND TABLE
 ; ==========================
+; list of executable commands
 command:
     mov edi,help_cmd
     call compare_strings
@@ -130,37 +138,39 @@ command:
 ; ==========================
 ; STRING COMPARE
 ; ==========================
+; compares the strings for command execution
 compare_strings:
     mov esi,buffer
 
 .loop:
-    mov al,[esi]
-    mov bl,[edi]
+    mov al,[esi]    ; loads the buffer
+    mov bl,[edi]    ; loads the command
 
-    cmp al,bl
-    jne .done
+    cmp al,bl       ; checks the command and buffer
+    jne .done       
 
-    cmp al, ' '
+    cmp al, ' '     ; for the load function because it is two words
     je .match
 
-    cmp al,0
+    cmp al,0        ; end of the string \ null terminator
     je .match
 
-    inc esi
-    inc edi
-    jmp .loop
+    inc esi         ; next entry
+    inc edi         ; next entry
+    jmp .loop       ; repeat
 
 .match:
-    mov edi, 1
+    mov edi, 1      ; sets flag true
     ret
 .done:
-    mov edi, 0
+    mov edi, 0      ; sets flag false
     ret
 
 
 ; ==========================
 ; PRINT STRING
 ; ==========================
+; print string routines
 print_string:
 .next:
     lodsb
@@ -196,6 +206,7 @@ clear_buffer:
 ; CLEAR SCREEN
 ; ==========================
 clear_screen:
+    ; resets the cursor and 0s the screen
     mov edi,0xB8000
     mov ecx,2000
     mov ax,0x0F20
@@ -210,6 +221,7 @@ clear_screen:
 ; NEW LINE
 ; ==========================
 newline:
+    ; moves the cursor along one line of the screen
     mov eax,[cursor]
 
     ; find current column
@@ -348,6 +360,8 @@ clear_cmd:
 load_cmd:
     db 'LOAD '
 
+
+
 help:
     mov esi,help_msg
     call print_string
@@ -373,9 +387,11 @@ load:
     call ata_read_sector
     call 0x30000
     ret
+
 ; ==========================
 ; FileTable Helper Commands
 ; ==========================
+
 find_file:
     ; number of files
     mov eax,[0x20004]
@@ -452,438 +468,7 @@ print_filetable:
 ; ==========================
 ; SCAN CODE TABLE
 ; ==========================
-keymap:
-times 0x10 db 0
-db 'Q'
-db 'W'
-db 'E'
-db 'R'
-db 'T'
-db 'Y'
-db 'U'
-db 'I'
-db 'O'
-db 'P'
-times (0x1E-0x1A) db 0
-db 'A'
-db 'S'
-db 'D'
-db 'F'
-db 'G'
-db 'H'
-db 'J'
-db 'K'
-db 'L'
-times (0x2C-0x27) db 0
-db 'Z'
-db 'X'
-db 'C'
-db 'V'
-db 'B'
-db 'N'
-db 'M'
 
-times 16384-($-$$) db 0org 0x7E00
-BITS 32
-
-start:
-    jmp get_inp
-
-; ==========================
-; KEYBOARD INPUT
-; ==========================
-get_inp:
-.wait:
-    in al,0x64
-    test al,1
-    jz .wait
-
-    in al,0x60
-
-    ; ignore key releases
-    test al,0x80
-    jnz get_inp
-
-    cmp al,0x1C
-    je enter_key
-
-    cmp al,0x0E
-    je backspace
-
-    cmp al,0x39
-    je spacebar
-
-    ; letters
-    movzx ebx,al
-    mov al,[keymap+ebx]
-
-    cmp al,0
-    je get_inp
-
-    call print_char
-    jmp get_inp
-
-
-; ==========================
-; ENTER
-; ==========================
-enter_key:
-    call newline
-    movzx edi,byte [buffer_pos]
-    mov byte [buffer+edi],0
-
-    call command
-    call clear_buffer
-
-    jmp get_inp
-
-; ==========================
-; BACKSPACE
-; ==========================
-backspace:
-    cmp byte [buffer_pos],0
-    je get_inp
-
-    dec byte [buffer_pos]
-    sub dword [cursor],2
-    mov edi,[cursor]
-
-    mov ax,0x0F20
-    mov [edi],ax
-
-    jmp get_inp
-
-
-; ==========================
-; SPACE
-; ==========================
-spacebar:
-    mov al,' '
-    call print_char
-    jmp get_inp
-
-
-; ==========================
-; PRINT CHARACTER
-; ==========================
-print_char:
-    push eax
-    mov edi,[cursor]
-    mov ah,0x0F
-    mov [edi],ax
-    add dword [cursor],2
-    pop eax
-    call store_char
-    ret
-
-
-; ==========================
-; STORE INPUT
-; ==========================
-store_char:
-    movzx edi,byte [buffer_pos]
-    mov [buffer+edi],al
-    inc byte [buffer_pos]
-    ret
-
-; ==========================
-; COMMAND TABLE
-; ==========================
-command:
-    mov edi,help_cmd
-    call compare_strings
-    cmp edi, 1
-    je help
-    mov edi,dir_cmd
-    call compare_strings
-    cmp edi, 1
-    je dir
-    mov edi,clear_cmd
-    call compare_strings
-    cmp edi, 1
-    je clear
-    mov edi,load_cmd
-    call compare_strings
-    cmp edi, 1
-    je load
-    ret
-
-; ==========================
-; STRING COMPARE
-; ==========================
-compare_strings:
-    mov esi,buffer
-
-.loop:
-    mov al,[esi]
-    mov bl,[edi]
-
-    cmp al,bl
-    jne .done
-
-    cmp al, ' '
-    je .match
-
-    cmp al,0
-    je .match
-
-    inc esi
-    inc edi
-    jmp .loop
-
-.match:
-    mov edi, 1
-    ret
-.done:
-    mov edi, 0
-    ret
-
-
-; ==========================
-; PRINT STRING
-; ==========================
-print_string:
-.next:
-    lodsb
-    cmp al,0
-    je .done
-    call print_char_screen
-    jmp .next
-.done:
-    ret
-; version without storing
-print_char_screen:
-    mov edi,[cursor]
-    mov ah,0x0F
-    mov [edi],ax
-    add dword [cursor],2
-    ret
-
-
-; ==========================
-; CLEAR BUFFER
-; ==========================
-clear_buffer:
-    mov edi,buffer
-    mov ecx,64
-    xor al,al
-.loop:
-    mov [edi],al
-    inc edi
-    loop .loop
-    mov byte [buffer_pos],0
-    ret
-; ==========================
-; CLEAR SCREEN
-; ==========================
-clear_screen:
-    mov edi,0xB8000
-    mov ecx,2000
-    mov ax,0x0F20
-.loop:
-    mov [edi],ax
-    add edi,2
-    loop .loop
-    mov dword [cursor],0xB8000
-    ret
-
-; ==========================
-; NEW LINE
-; ==========================
-newline:
-    mov eax,[cursor]
-
-    ; find current column
-    sub eax,0xB8000
-    xor edx,edx
-    mov ecx,160
-    div ecx          ; eax = row, edx = offset in row
-
-    ; move to next row
-    sub dword [cursor],edx
-    add dword [cursor],160
-
-    ret
-
-; ==========================
-; ATA DRIVER
-; ==========================
-
-ata_wait_busy:
-.wait:
-    mov dx, 0x1F7
-    in  al, dx
-    test al, 0x80            ; BSY
-    jnz .wait
-    ret
-
-ata_wait_drq:
-.wait:
-    mov dx, 0x1F7
-    in  al, dx
-
-    test al, 0x01            ; ERR
-    jnz .error
-
-    test al, 0x80            ; BSY
-    jnz .wait
-
-    test al, 0x08            ; DRQ
-    jz .wait
-
-    ret
-
-.error:
-    mov al,'E'
-    call print_char_screen
-.hang:
-    jmp .hang
-
-ata_read_sector:
-
-    push eax
-    push ebx
-    push ecx
-    push edx
-
-    mov ebx,eax
-
-    call ata_wait_busy
-
-    ; sector count
-    mov dx,0x1F2
-    mov al,1
-    out dx,al
-
-    ; LBA 0-7
-    mov dx,0x1F3
-    mov al,bl
-    out dx,al
-
-    ; LBA 8-15
-    mov dx,0x1F4
-    mov al,bh
-    out dx,al
-
-    ; LBA 16-23
-    shr ebx,16
-    mov dx,0x1F5
-    mov al,bl
-    out dx,al
-
-    ; LBA 24-27
-    shr ebx,8
-    mov dx,0x1F6
-    mov al,bl
-    and al,0x0F
-    or al,0xE0
-    out dx,al
-
-    ; read command
-    mov dx,0x1F7
-    mov al,0x20
-    out dx,al
-
-    call ata_wait_drq
-
-    mov dx,0x1F0
-    mov ecx,256
-
-.read:
-    in ax,dx
-    mov [edi],ax
-    add edi,2
-    loop .read
-
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    ret
-
-
-; ==========================
-; DATA
-; ==========================
-cursor:
-    dd 0xB8000
-
-buffer_pos:
-    db 0
-buffer:
-    times 64 db 0
-
-
-; ==========================
-; COMMANDS
-; ==========================
-
-help_cmd:
-    db 'HELP',0
-help_msg:
-    db 'CLEAR DIR HELP LOAD ',0
-dir_cmd:
-    db 'DIR',0
-clear_cmd:
-    db 'CLEAR',0
-load_cmd:
-    db 'LOAD '
-
-help:
-    mov esi,help_msg
-    call print_string
-    call newline
-    ret
-
-clear:
-    call clear_screen
-    ret
-
-dir:
-    mov eax,33
-    mov edi,0x20000
-    call ata_read_sector
-
-    call print_filetable
-
-    ret
-
-load:
-    mov eax, 34
-    mov edi,0x30000
-    call ata_read_sector
-    call 0x30000
-    ret
-; ==========================
-; FileTable Helper Commands
-; ==========================
-
-
-print_filetable:
-    mov eax,[0x20004]     ; file count
-    xor ebx,ebx           ; index
-
-.loop:
-    cmp ebx,eax
-    je .done
-
-    mov esi,0x20008       ; first entry
-    mov edx,ebx
-    imul edx,32           ; entry size
-    add esi,edx
-
-    call print_string
-
-    inc ebx
-    jmp .loop
-
-.done:
-    call newline
-    ret
-
-; ==========================
-; SCAN CODE TABLE
-; ==========================
 keymap:
 times 0x10 db 0
 db 'Q'
