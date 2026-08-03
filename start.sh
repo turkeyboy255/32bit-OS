b08ce1dc10b1e48799dda144af763a54e45b240b
turkeyboy255 <turkey.boyz2010@gmail.com>
2026-08-03 00:25:38 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ nasm -f bin Bootloader.asm -o Bootloader.bin
 nasm -f bin Kernel32.asm -o Kernel32.bin
 nasm -f bin filetable.asm -o filetable.bin
 nasm -f bin hello.asm -o hello.bin
+nasm -f bin syscall.asm -o syscall.bin
 
 
 dd if=/dev/zero of=os.img bs=512 count=2880
@@ -19,17 +20,6 @@ dd if=filetable.bin of=os.img bs=512 seek=33 conv=notrunc
 dd if=hello.bin of=os.img bs=512 seek=34 conv=notrunc
 
 
-qemu-system-i386 -drive format=raw,file=os.imgnasm -f bin Bootloader.asm -o Bootloader.bin
-nasm -f bin Kernel32.asm -o Kernel32.bin
-nasm -f bin filetable.asm -o filetable.bin
-
-
-
-dd if=/dev/zero of=os.img bs=512 count=2880
-
-dd if=Bootloader.bin of=os.img bs=512 count=1 conv=notrunc
-dd if=Kernel32.bin of=os.img bs=512 seek=1 conv=notrunc
-
-dd if=filetable.bin of=os.img bs=512 seek=33 conv=notrunc
+dd if=syscall.bin of=os.img bs=512 seek=36 conv=notrunc
 
 qemu-system-i386 -drive format=raw,file=os.img
